03109fbb03d65d8b96590bd13c4e0118b090cd27
camilafbc <camilafernanda914@gmail.com>
2023-05-29 23:18:52 -0300

    first commit

---
+++ buzzfeed/src/app/components/quizz/quizz.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quizz',
+  templateUrl: './quizz.component.html',
+  styleUrls: ['./quizz.component.css']
+})
+export class QuizzComponent {
+
+title:string = ""
+
+questions:any = ""
+questionSelected:any = ""
+
+answers:string[] = []
+answerSelected:string = ""
+
+questionIndex:number = 0
+questionMaxIndex:number = 0
+
+finished:boolean = false
+
+}
